2f5a12fc467da3b2e84e46dd109f7a80fdbc80af
Aidan Fu <aidan.fu000@gmail.com>
2022-11-27 22:09:05 -0500

    convert from dependency function to context manager for session use

--- messenger_schemas/schema/__init__.py
+++ messenger_schemas/schema/__init__.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from typing import Any
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
@@ -27,14 +28,17 @@ class BaseRecord:
         return self.key() == other.key()
 
 
-def database_session():
+def make_engine():
     engine = create_engine(
         RDS_URL,
         pool_size=RDS_POOL_SIZE,
         max_overflow=RDS_MAX_OVERFLOW,
         connect_args={"connect_timeout": RDS_CONNECTION_TIMEOUT},
     )
+    return engine
 
+
+def database_session(engine):
     session: Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
 
     try:
@@ -46,3 +50,15 @@ def database_session():
         logger.error("an error occured while using the database session due to %s", e)
     finally:
         session.close()
+
+
+def get_db_session_context():
+    engine = make_engine()
+
+    def get_database_session():
+        return database_session(engine)
+
+    return contextmanager(get_database_session)
+
+
+DatabaseSession = get_db_session_context()
